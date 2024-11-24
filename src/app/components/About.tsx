9905f6e2033bc8a9eb1fd4e9d1822fafd7001907
jallaluddin <jalaluddin7096@gmail.com>
2024-11-25 00:13:18 +0500

    first commit

---
+++ src/app/components/About.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <section style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
+        <h2>About This App</h2>
+        <p>This Next.js app welcomes you to demonstrate components in a React-based framework.</p>
+      </section>
+    );
+  }
+  
---
+++ src/app/components/Header..tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <header style={{ backgroundColor: '#0070f3', color: '#fff', padding: '1rem' }}>
+        <h1>You're all warmly welcome!</h1>
+      </header>
+    );
+  }
+  
